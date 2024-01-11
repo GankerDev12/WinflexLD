aa5c7a1bb35bb6cf4b4ea829005b888bfc83acab
Joshua Cid <joshua.cid1985@gmail.com>
2024-01-11 17:52:11 -0300

    Inicio servidor

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
