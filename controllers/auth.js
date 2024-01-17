fd21e2c6a1df38abca7e18d143c4f20c2b6603c7
Joshua Cid <joshua.cid1985@gmail.com>
2024-01-17 12:33:06 -0300

    Add rutas usuario

---
+++ controllers/auth.js
@@ -0,0 +1,39 @@
+const { response } = require('express');
+
+
+const crearUsuario = (req, res = response) => {
+    const { name, email, password } = req.body;
+
+    res.status(201).json({
+        ok: true,
+        msg: 'registro',
+        name,
+        email,
+        password
+    })
+}
+
+const login = (req, res = response) => {
+    const { email, password } = req.body;
+
+    res.json({
+        ok: true,
+        msg: 'login',
+        email,
+        password
+    })
+}
+
+const revalidarToken = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'revalidar token'
+    })
+}
+
+
+module.exports = {
+    crearUsuario,
+    login,
+    revalidarToken
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,2 +1,29 @@
+/*
+    Rutas de usuarios / Auth
+    host + /api/auth
+*/
 const { Router } = require('express');
+const router = Router();
+const { check } = require('express-validator');
 
+const { crearUsuario, login, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar_campos');
+
+router.post('/register', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'Ingresa un email valido').isEmail(),
+    check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
+],
+    crearUsuario);
+
+router.post('/', [
+    check('email', 'Email es obligatorio').isEmail(),
+    check('password', 'Password es obligatorio').isLength({ min: 6 }),
+    validarCampos
+],
+    login);
+
+router.get('/renew', revalidarToken);
+
+module.exports = router;
