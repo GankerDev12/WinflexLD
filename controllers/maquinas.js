468fc8ac3c08f56a45067ddbfca269631bb02517
Joshua Cid <joshua.cid1985@gmail.com>
2024-01-19 14:23:00 -0300

    Add rutas, controllers para maquina model

---
+++ controllers/maquinas.js
@@ -0,0 +1,118 @@
+const { response } = require('express');
+const Maquina = require('../models/Maquina');
+
+const getMaquinas = async (req, res = response) => {
+    const maquinas = await Maquina.find()
+        .populate('user', ' name');
+
+    res.json({
+        ok: true,
+        maquinas
+    });
+}
+
+const crearMaquina = async (req, res = response) => {
+    const maquina = new Maquina(req.body);
+
+    try {
+        let maquinab = await Maquina.findOne({ nombre: maquina.nombre });
+        if (maquinab) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Esta maquina ya esta creada'
+            })
+        }
+
+        maquina.user = req.uid;
+
+        const maquinaGuardada = await maquina.save();
+
+        res.json({
+            ok: true,
+            maquina: maquinaGuardada
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+}
+
+const actualizarMaquina = async (req, res = response) => {
+    const maquinaId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const maquina = await Maquina.findById(maquinaId);
+        if (!maquina) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Maquina no encontrada con ese id'
+            });
+        }
+        const nuevaMaquina = {
+            ...req.body,
+            user: uid
+        }
+        const maquinaActualizada = await Maquina.findByIdAndUpdate(maquinaId, nuevaMaquina)
+            .populate('user', 'name');
+        res.json({
+            ok: true,
+            maquina: maquinaActualizada
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+
+    res.json({
+        ok: true,
+        maquinaId
+    });
+}
+
+const eliminarMaquina = async (req, res = response) => {
+    const maquinaId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const maquina = await Maquina.findById(maquinaId);
+        if (!maquina) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Maquina no encontrada con ese id'
+            });
+        }
+        await Maquina.findByIdAndDelete(maquinaId)
+
+        res.json({
+            ok: true,
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+
+    res.json({
+        ok: true,
+        maquinaId
+    });
+}
+
+module.exports = {
+    getMaquinas,
+    crearMaquina,
+    actualizarMaquina,
+    eliminarMaquina
+}
--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
 const { dbConnection } = require('./database/config');
 
-require('dotenv').config();
 
 const app = express();
 
 //Base de datos
 dbConnection();
+//CORS
+app.use(cors());
 
 
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/maquinas', require('./routes/maquinas'));
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
