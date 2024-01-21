5e4124e4406b3fd3dff7c74de3223624c736621d
Joshua Cid <joshua.cid@virginiogomez.cl>
2024-01-21 17:07:54 -0300

    add rutas auth

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
-
-
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.json());
+
+
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
--- routes/auth.js
+++ routes/auth.js
@@ -1,2 +1,13 @@
 const { Router } = require('express');
+const router = Router();
 
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
+
+
+router.post('/register', createUser);
+
+router.post('/', loginUser);
+
+router.get('/renew', renewToken);
+
+module.exports = router;
